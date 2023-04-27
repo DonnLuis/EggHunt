6df738ee5014c88fe4be37ea67ef2d4d8e3421e8
DonnLuis <devstuffin@gmail.com>
2023-04-26 19:05:13 -0600

    added some notes to the ip/subnet/subnet.go file for later usage

--- ip/subnet/subnet.go
+++ ip/subnet/subnet.go
@@ -6,7 +6,8 @@ import (
 )
 
 type Subnet struct {
-	sub []string
+	ip     string
+	subnet int
 }
 
 func IP(ip string, subnet int) {
@@ -14,3 +15,31 @@ func IP(ip string, subnet int) {
 	cidr := ip + "/" + sub
 	fmt.Println(cidr)
 }
+
+func SubnetScan(cidr string) {
+
+}
+
+/*
+
+
+
+
+	I'm still working on creating the appropriate functions to scan a subnet entered by the user.
+
+	if a user enters a cidr of 192.168.0.0/24, I need to parse and scan the ip based on the subnet, where that is a 24,32, etc..
+
+	should I use BufferReaders/Writers for this process? using the standard library: bufio.
+
+
+
+
+
+
+
+
+
+
+
+
+*/
